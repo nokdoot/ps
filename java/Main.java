e7c599c91d5ea23686c1ff01941d988cb51165f6
nokdoot <nokdoot@naver.com>
2019-10-18 08:25:17 +0900

    Main.java

---
+++ java/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static String line;
+    static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        Question();
+    }
+
+    public static void Question() throws IOException {
+    }
+}
