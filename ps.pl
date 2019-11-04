6a8dc9a9d1cdf75e6a2baa70e312c696d80899b0
nokdoot <nokdoot@naver.com>
2019-11-04 22:01:48 +0900

    ps:

--- ps.pl
+++ ps.pl
@@ -3,32 +3,76 @@
 use strict;
 use warnings;
 use feature qw/ say /;
-   
-chomp (my $line = <STDIN>);
 
-my $q = $line;
+my @tokens = ();
 
-for (1..$q) {
-    chomp ($line = <STDIN>);
-    my ($n, $r) = split q{ }, $line;
-    chomp ($line = <STDIN>);
-    my @monsters = split q{ }, $line;
+my $n = read_token();
 
-    my $set = {};
-    for (@monsters) {
-        $set->{$_} = 1 if !defined $set->{$_};
+my @locations = ();
+for ( 1..$n ) {
+    push @locations, read_token();
+}
+
+my $l = 1;
+my $r = ($n-1) * $l;
+
+my $min = abs($locations[1] - $l) + abs($locations[-1] - $r);
+
+while ( 1 ) {
+    $l++;
+    $r = ($n-1) * $l;
+    my $new_min = abs($locations[1] - $l) + abs($locations[-1] - $r);
+    if ( $new_min < $min ) {
+        $min = $new_min;
+        next;
     }
+    $l--;
+    last;
+}
 
-    @monsters = sort { $a <=> $b } keys %$set;
+my $answer = 0;
+shift @locations;
+my $i = 1;
+while (@locations) {
+    $answer += abs ( $i * $l - (shift @locations) );
+    $i++;
+}
 
-    my $answer = 0;
-    my $i = @monsters-1;
-    while ( $i >= 0 ) {
-        if ( $monsters[$i] > $answer * $r ) {
-            $answer++;
-        }
-        $i--;
+say $answer;
+
+sub read_line {
+    chomp (my $line = <STDIN>);
+    return $line;
+}
+
+sub read_token {
+    @tokens = split q{ }, read_line() if @tokens == 0;
+    return shift @tokens;
+}
+
+sub min {
+    my @numbers = @_;
+    my $min = shift @numbers;
+    for ( @numbers ) {
+        $min = $min<$_ ? $min : $_;
     }
+    return $min;
+}
 
-    say $answer;
+sub max {
+    my @numbers = @_;
+    my $max = shift @numbers;
+    for ( @numbers ) {
+        $max = $max > $_ ? $max : $_;
+    }
+    return $max;
+}
+
+sub sum {
+    my @numbers = @_;
+    my $sum = 0;
+    for (@numbers) {
+        $sum += $_;
+    }
+    return $sum;
 }
